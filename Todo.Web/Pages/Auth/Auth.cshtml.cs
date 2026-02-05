2a8fa86425e2fcfc4c49ac15ae08186e990157f3
alireza-haeri <alireza.haeri.dev@gmail.com>
2026-02-05 10:03:51 +0330

    create empty pages in Todo.Web project

---
+++ Todo.Web/Pages/Auth/Auth.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Todo.Web.pages.Auth;
+
+public class Auth : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ Todo.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Todo.Web.pages;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
